89a43da380afd8ed2de5a3a450f534ef3827f721
Park-Sin-Ae <52437656+Park-Sin-Ae@users.noreply.github.com>
2022-12-22 06:04:41 +0900

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-
-$(function(){
-	$('.gr_ps').animate({width: '95%'}, 2000);
-	$('.gr_html').animate({width: '95%'}, 1500);
-	$('.gr_jquery').animate({width: '60%'}, 2000);
-	$('.gr_illu').animate({width: '80%'},1500);
-});
